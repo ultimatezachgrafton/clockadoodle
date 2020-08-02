ed3d501b2c346bb0d1ced84901c024c098f1a1fe
ultimatezachgrafton <arrowny@aol.com>
2020-08-01 21:02:24 -0700

    Add files via upload
    Updated code for legibility and efficiency.

--- dom.js
+++ dom.js
@@ -1,58 +1,40 @@
-var today = new Date();
-var hour;
-var minutes;
-var seconds;
-var meridies;
-var timeStandard = "00:00:00 --";
-var timeMilitary = "00:00:00";
-var btn = document.getElementById("btn-time-format");
-var btnInterval = setInterval(incrementStandard, 1000);
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let isMilitaryTime = false;
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-btn.innerHTML = "Click for Military Time", btn.value = "Standard Time", 
-    btn.addEventListener("click", changeTimeFormat);
+renderClock();
+let btnInterval = setInterval(renderClock, 1000);
 
-function updateTimeStandard(hour, minutes, seconds) {
-    today = new Date();
-    hour = today.getHours();
-    minutes = ('0'+today.getMinutes()).slice(-2);
-    seconds = ('0'+today.getSeconds()).slice(-2);
-    if (hour > 12) { meridies="PM"; hour = (hour-12); } else { meridies="AM";}
-    getDay();
-    return timeStandard = hour + ":" + minutes + ":" + seconds + " " + meridies;
-}
-
-function updateTimeMilitary(hour, minutes, seconds) {
-    today = new Date();
-    hour = today.getHours();
-    minutes = ('0'+today.getMinutes()).slice(-2);
-    seconds = ('0'+today.getSeconds()).slice(-2);
-    getDay();
-    return timeMilitary = hour + ":" + minutes + ":" + seconds;
-}
-
-function incrementStandard() {
-    timeStandard = updateTimeStandard(hour, minutes, seconds);
-    document.getElementById("h1-clock").innerHTML = timeStandard;
-}
-
-function incrementMilitary() {
-    timeMilitary = updateTimeMilitary(hour, minutes, seconds);
-    document.getElementById("h1-clock").innerHTML = timeMilitary;
-}
-
-function changeTimeFormat() {
-    if (btn.value=="Standard Time") { 
-        btn.innerHTML = "Click for Standard Time", btn.value = "Military Time" ,
-        clearInterval(btnInterval),
-        btnInterval = setInterval(incrementMilitary, 1000);
-    } else { btn.innerHTML = "Click for Military Time", btn.value = "Standard Time",
-        clearInterval(btnInterval),
-        btnInterval = setInterval(incrementStandard, 1000);
+const btn = document.getElementById("btn-time-format");
+btn.innerHTML = "Click for Military Time";
+btn.value = "Standard Time";
+btn.addEventListener("click", function(e) {
+    if (isMilitaryTime === false) { 
+        btn.innerHTML = "Click for Standard Time";
+        isMilitaryTime = true;
+    } else { 
+        btn.innerHTML = "Click for Military Time";
+        isMilitaryTime = false;
     };
-}
+    renderClock();
+});
 
-function getDay() {
-    document.getElementById("h2-date").innerHTML = days[today.getDay()] + ", " +  months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+function renderClock() {
+    const today = new Date();
+
+    document.getElementById("h2-date").innerHTML = 
+        days[today.getDay()] + ", " +  months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+
+    const hour = today.getHours();
+    const minutes = ('0'+today.getMinutes()).slice(-2);
+    const seconds = ('0'+today.getSeconds()).slice(-2);
+
+    if (isMilitaryTime) {
+        document.getElementById("h1-clock").innerHTML = 
+            `${hour}:${minutes}:${seconds}`;
+        return;
+    } 
+    
+    document.getElementById("h1-clock").innerHTML =
+        `${hour > 12 ? hour - 12 : hour}:${minutes}:${seconds} ${hour > 12 ? "PM" : "AM"}`;
 }
